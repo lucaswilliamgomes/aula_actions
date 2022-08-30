1b68d9f33d7af6051eec5ab8e4ad54cded455f04
Lucas William <lucaswilliam133@gmail.com>
2022-08-30 00:09:54 -0300

    edit app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula GitHub Actions
+          Aula GitHub Actions - Lucas#1
         </p>
         <a
           className="App-link"
